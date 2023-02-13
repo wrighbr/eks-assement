2c594f2c4a4e65d16cdaf734cf861ea21d877cf4
Brett Wright <wright.brett@gmail.com>
2023-02-13 16:28:04 +1100

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -19,8 +19,8 @@ resource "aws_eks_node_group" "node" {
   subnet_ids      = var.subnet_ids
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 3
+    max_size     = 10
     min_size     = 1
   }
 
